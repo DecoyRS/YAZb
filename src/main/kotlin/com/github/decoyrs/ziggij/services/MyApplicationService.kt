4da0c99b0d5d154582a699d7206c9d8b710b524b
GitHub Action <action@github.com>
2021-05-20 11:36:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/decoyrs/ziggij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/decoyrs/ziggij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.decoyrs.ziggij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.decoyrs.ziggij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/decoyrs/ziggij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/decoyrs/ziggij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.decoyrs.ziggij.services
 
+import com.github.decoyrs.ziggij.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
