22e62e56b645386d6ea1e0b96ac0fe981517bf43
Alexander Pirogov <a.g.pirogov@gmail.com>
2021-06-21 15:54:17 +0300

    Fix deprecation warning in ZigIcons

--- src/main/kotlin/Icons/ZigIcons.kt
+++ src/main/kotlin/Icons/ZigIcons.kt
@@ -1,11 +1,12 @@
 package Icons
 
 import com.intellij.openapi.util.IconLoader
+import com.intellij.util.IconUtil
 import javax.swing.Icon
 
 object ZigIcons {
-    val FILE_ICON = IconLoader.getIcon("/Icons/zig_file.png")
-    val ZIG_BIG_ICON = IconLoader.getIcon("/Icons/zig_icon.png")
-    val ZIG_VAR = IconLoader.getIcon("/Icons/zig_variable.png")
-    val ZIG_FUN = IconLoader.getIcon("/Icons/zig_function.png")
+    val FILE_ICON = IconLoader.getIcon("/Icons/zig_file.png", IconUtil::class.java)
+    val ZIG_BIG_ICON = IconLoader.getIcon("/Icons/zig_icon.png", IconUtil::class.java)
+    val ZIG_VAR = IconLoader.getIcon("/Icons/zig_variable.png", IconUtil::class.java)
+    val ZIG_FUN = IconLoader.getIcon("/Icons/zig_function.png", IconUtil::class.java)
 }
