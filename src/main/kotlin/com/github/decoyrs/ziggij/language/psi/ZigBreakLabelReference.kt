164edf90524f22be512ab6fb9771bdb2bec21d2e
Alexander Pirogov <a.g.pirogov@gmail.com>
2021-07-19 15:50:10 +0300

    Code completion and navigation from label usage to label decl

--- src/main/kotlin/com/github/decoyrs/ziggij/language/psi/ZigBreakLabelReference.kt
+++ src/main/kotlin/com/github/decoyrs/ziggij/language/psi/ZigBreakLabelReference.kt
@@ -1,6 +1,9 @@
 package com.github.decoyrs.ziggij.language.psi
 
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.PsiPolyVariantReferenceBase
 import com.intellij.psi.ResolveResult
@@ -16,6 +19,42 @@ class ZigBreakLabelReference(private val zigBreakLabel: ZigBreakLabel) :
 
     override fun resolve() = multiResolve(false).firstOrNull()?.element as? ZigElement
 
+    override fun isReferenceTo(element: PsiElement): Boolean {
+        return element is ZigBlockLabel && element.symbolDecl.text == zigBreakLabel.symbol.text && element.symbolDecl === resolve()
+    }
+
+    override fun getVariants(): Array<LookupElement> {
+        val file = element.containingFile ?: return emptyArray()
+        if (element.isValid.not()) return emptyArray()
+
+        val result = arrayListOf<LookupElement>()
+        treeWalkUp(element, file) { scope, _ ->
+            if (scope !is ZigElement) {
+                return@treeWalkUp false
+            }
+            when (scope) {
+                is ZigBlockExpr -> scope.blockLabel?.let {
+                    val lookupElement = LookupElementBuilder.create(it)
+                    result.add(lookupElement)
+                }
+                is ZigPrimaryExpr -> scope.blockLabel?.let {
+                    val lookupElement = LookupElementBuilder.create(it)
+                    result.add(lookupElement)
+                }
+                is ZigLabeledStatement -> scope.blockLabel?.let {
+                    val lookupElement = LookupElementBuilder.create(it)
+                    result.add(lookupElement)
+                }
+                is ZigLabeledTypeExpr -> scope.blockLabel?.let {
+                    val lookupElement = LookupElementBuilder.create(it)
+                    result.add(lookupElement)
+                }
+            }
+            true
+        }
+        return result.toTypedArray()
+    }
+
     override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> {
         val file = element.containingFile ?: return emptyArray()
         if (element.isValid.not()) return emptyArray()
@@ -26,16 +65,16 @@ class ZigBreakLabelReference(private val zigBreakLabel: ZigBreakLabel) :
                 return@treeWalkUp false
             }
             when (scope) {
-                is ZigBlockExpr -> if (scope.blockLabel != null) {
+                is ZigBlockExpr -> if (scope.blockLabel?.symbolDecl?.text == zigBreakLabel.symbol.text) {
                     result.add(PsiElementResolveResult(scope.blockLabel!!.symbolDecl))
                 }
-                is ZigPrimaryExpr -> if (scope.blockLabel != null) {
+                is ZigPrimaryExpr -> if (scope.blockLabel?.symbolDecl?.text == zigBreakLabel.symbol.text) {
                     result.add(PsiElementResolveResult(scope.blockLabel!!.symbolDecl))
                 }
-                is ZigLabeledStatement -> if (scope.blockLabel != null) {
+                is ZigLabeledStatement -> if (scope.blockLabel?.symbolDecl?.text == zigBreakLabel.symbol.text) {
                     result.add(PsiElementResolveResult(scope.blockLabel!!.symbolDecl))
                 }
-                is ZigLabeledTypeExpr -> if (scope.blockLabel != null) {
+                is ZigLabeledTypeExpr -> if (scope.blockLabel?.symbolDecl?.text == zigBreakLabel.symbol.text) {
                     result.add(PsiElementResolveResult(scope.blockLabel!!.symbolDecl))
                 }
             }
